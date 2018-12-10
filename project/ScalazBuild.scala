3846684b05abd1bd13cc293a996be41ecd6bc53d
Dejan Mijic <dmijic@acm.org>
2018-12-10 12:02:02 +0100

    Bump scala version to 2.12.8

--- project/ScalazBuild.scala
+++ project/ScalazBuild.scala
@@ -51,7 +51,7 @@ object Scalaz {
   def stdSettings(prjName: String) = Seq(
     name := s"scalaz-$prjName",
     scalacOptions := stdOptions,
-    crossScalaVersions := Seq("2.12.6", "2.11.12"),
+    crossScalaVersions := Seq("2.12.8", "2.11.12"),
     scalaVersion in ThisBuild := crossScalaVersions.value.head,
     scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
     libraryDependencies ++= compileOnlyDeps ++ testDeps ++ Seq(
