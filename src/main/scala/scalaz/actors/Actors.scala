12d1372593e724fe2ca9733c4fac1f19e75caed8
Salar Rahmanian <code@softinio.com>
2018-12-19 20:29:29 -0800

    Initial refactor wip

--- src/main/scala/scalaz/actors/Actors.scala
+++ src/main/scala/scalaz/actors/Actors.scala
@@ -2,59 +2,43 @@ package scalaz.actors
 
 import scalaz.zio.{ IO, Promise, Queue, Ref, Schedule }
 
-object Actors {
+trait Actor[+E, -F[+ _]] {
+  def ![A](fa: F[A]): IO[E, A]
 
-  trait Actor[+E, -F[+ _]] {
-    def ![A](fa: F[A]): IO[E, A]
-    def stop(): IO[Nothing, Unit]
-  }
+  val stop: IO[Nothing, Unit]
+}
 
-  trait MessageHandler[S, +E, -F[+ _]] {
+object Actor {
+  val DefaultActorMailboxSize = 10000
+
+  trait Stateful[S, +E, -F[+ _]] {
     def receive[A](state: S, msg: F[A]): IO[E, (S, A)]
   }
 
-  trait Supervisor[E] {
-    def supervise[A](io: IO[E, A], error: E): IO[E, A]
-  }
-
-  object Supervisor {
-
-    def none: Supervisor[Nothing] = retry(Schedule.never)
-
-    def retry[E](policy: Schedule[E, _]): Supervisor[E] =
-      new Supervisor[E] {
-
-        def supervise[A](io: IO[E, A], error: E): IO[E, A] =
-          io.retry(policy)
-      }
-  }
-
-  val QueueSize = 10000
-
-  type PendingMessage[E, F[_], A] = (F[A], Promise[E, A])
-
-  def makeActor[S, E, F[+ _]](initial: S)(
-    messageHandler: MessageHandler[S, E, F]
-  )(supervisor: Supervisor[E]): IO[Nothing, Actor[E, F]] = {
+  final def stateful[S, E, F[+ _]](supervisor: Supervisor[E],
+                                   mailboxSize: Int = DefaultActorMailboxSize)(initial: S)(
+    stateful: Actor.Stateful[S, E, F]
+  ): IO[Nothing, Actor[E, F]] = {
+    type PendingMessage[E, F[_], A] = (F[A], Promise[E, A])
 
     def process[A](msg: PendingMessage[E, F, A], state: Ref[S]): IO[Nothing, Unit] =
       for {
         s             <- state.get
         (fa, promise) = msg
-        receiver      = messageHandler.receive(s, fa)
+        receiver      = stateful.receive(s, fa)
         completer     = ((s: S, a: A) => state.set(s) *> promise.complete(a)).tupled
         _ <- receiver.redeem(
               e =>
                 supervisor
                   .supervise(receiver, e)
-                  .redeem(promise.error, completer),
+                  .redeem(_ => promise.error(e), completer),
               completer
             )
       } yield ()
 
     for {
       state <- Ref(initial)
-      queue <- Queue.bounded[PendingMessage[E, F, _]](QueueSize)
+      queue <- Queue.bounded[PendingMessage[E, F, _]](mailboxSize)
       fiber <- (for {
                 t <- queue.take
                 _ <- process(t, state)
@@ -67,9 +51,23 @@ object Actors {
             _       <- queue.offer((a, promise))
             value   <- promise.get
           } yield value
-        override def stop(): IO[Nothing, Unit] =
-          fiber.interrupt *> IO.unit
-        // TODO queue.shutdown
+        // TODO: Shutdown queue
+        override val stop: IO[Nothing, Unit] = fiber.interrupt *> IO.unit
       }
   }
+}
+
+trait Supervisor[-E] {
+  def supervise[A](io: IO[E, A], error: E): IO[Unit, A]
+}
+
+object Supervisor {
+  final def none: Supervisor[Any] = retry(Schedule.never)
+
+  final def retry[E](policy: Schedule[E, _]): Supervisor[E] =
+    new Supervisor[E] {
+
+      def supervise[A](io: IO[E, A], error: E): IO[Unit, A] =
+        io.retry(policy).leftMap(_ => ())
+    }
 }
