a3cacd8c4f0e126018f9213392c505eeb0f40c87
Dejan Mijic <dmijic@acm.org>
2019-03-28 23:39:04 +0100

    Optimize imports

--- src/main/scala/scalaz/actors/Actor.scala
+++ src/main/scala/scalaz/actors/Actor.scala
@@ -1,9 +1,6 @@
 package scalaz.actors
 
-import scala.collection.immutable.List
-
-import scalaz.zio.{ IO, Promise, Queue, Ref, Schedule }
-import scalaz.zio.clock.Clock
+import scalaz.zio.{ IO, Promise, Queue, Ref }
 
 trait Actor[+E, -F[+ _]] {
   def ![A](fa: F[A]): IO[E, A]
@@ -65,17 +62,3 @@ object Actor {
   }
 }
 
-trait Supervisor[-E] {
-  def supervise[A](io: IO[E, A], error: E): IO[Unit, A]
-}
-
-object Supervisor {
-  final def none: Supervisor[Any] = retry(Schedule.never)
-
-  final def retry[E](policy: Schedule[E, _]): Supervisor[E] =
-    new Supervisor[E] {
-
-      def supervise[A](io: IO[E, A], error: E): IO[Unit, A] =
-        io.retry(policy).mapError(_ => ()).provide(Clock.Live)
-    }
-}
---
+++ src/main/scala/scalaz/actors/Supervisor.scala
@@ -0,0 +1,19 @@
+package scalaz.actors
+
+import scalaz.zio.{ IO, Schedule }
+import scalaz.zio.clock.Clock
+
+trait Supervisor[-E] {
+  def supervise[A](io: IO[E, A], error: E): IO[Unit, A]
+}
+
+object Supervisor {
+  final def none: Supervisor[Any] = retry(Schedule.never)
+
+  final def retry[E](policy: Schedule[E, _]): Supervisor[E] =
+    new Supervisor[E] {
+
+      def supervise[A](io: IO[E, A], error: E): IO[Unit, A] =
+        io.retry(policy).mapError(_ => ()).provide(Clock.Live)
+    }
+}
