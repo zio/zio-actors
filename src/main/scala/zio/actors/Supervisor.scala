412ca6448fc3dc61744f604259fe2fa02971ffb4
Dejan Mijic <dmijic@acm.org>
2019-05-03 00:25:55 +0200

    Draft solution

--- src/main/scala/zio/actors/Supervisor.scala
+++ src/main/scala/zio/actors/Supervisor.scala
@@ -2,18 +2,25 @@ package zio.actors
 
 import scalaz.zio.{ IO, Schedule }
 import scalaz.zio.clock.Clock
+import scalaz.zio.ZIO
 
 trait Supervisor[-E] {
-  def supervise[A](io: IO[E, A], error: E): IO[Unit, A]
+  type A0
+
+  def supervise(io: IO[E, A0], error: E): IO[Unit, A0]
 }
 
 object Supervisor {
   final def none: Supervisor[Any] = retry(Schedule.never)
 
-  final def retry[E](policy: Schedule[E, _]): Supervisor[E] =
-    new Supervisor[E] {
+  final def retry[E, A](policy: Schedule[E, A]): Supervisor[E] =
+    retryOrElse(policy, (e: E, _: A) => ZIO.fail(e))
 
-      def supervise[A](io: IO[E, A], error: E): IO[Unit, A] =
-        io.retry(policy).mapError(_ => ()).provide(Clock.Live)
+  final def retryOrElse[E, A](policy: Schedule[E, A], orElse: (E, A) => IO[E, A]): Supervisor[E] =
+    new Supervisor[E] {
+      override type A0 = A
+
+      override def supervise(io: IO[E, A0], error: E): IO[Unit, A0] = 
+        io.retryOrElse(policy, orElse).mapError(_ => ()).provide(Clock.Live)
     }
 }
