cc79e861bff8533ff80e0303f8bbbfc3102282b2
Dejan Mijic <dmijic@acm.org>
2019-05-03 21:48:05 +0200

    Test retryOrElse implementation

--- src/main/scala/zio/actors/Supervisor.scala
+++ src/main/scala/zio/actors/Supervisor.scala
@@ -10,12 +10,17 @@ trait Supervisor[-E] {
 object Supervisor {
   final def none: Supervisor[Any] = retry(Schedule.never)
 
-  final def retry[E, A](policy: Schedule[E, A]): Supervisor[E] = 
+  final def retry[E, A](policy: Schedule[E, A]): Supervisor[E] =
     retryOrElse(policy, (_: E, _: A) => IO.unit)
 
-  final def retryOrElse[E, A](policy: Schedule[E, A], orElse: (E, A) => IO[E, Unit]): Supervisor[E] =
+  final def retryOrElse[E, A](
+    policy: Schedule[E, A],
+    orElse: (E, A) => IO[E, Unit]
+  ): Supervisor[E] =
     new Supervisor[E] {
-      override def supervise[A0](io: IO[E, A0], error: E): IO[Unit, A0] = 
-        io.retryOrElse(policy, (e: E, a: A) => orElse(e, a) *> ZIO.fail(error)).mapError(_ => ()).provide(Clock.Live)
+      override def supervise[A0](io: IO[E, A0], error: E): IO[Unit, A0] =
+        io.retryOrElse(policy, (e: E, a: A) => orElse(e, a) *> ZIO.fail(error))
+          .mapError(_ => ())
+          .provide(Clock.Live)
     }
 }
